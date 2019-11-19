efce517e1d1f26096738aa20b2b5911997b9a45e
Nina Jensen <jnik@aarhus.dk>
2019-11-19 12:52:12 +0100

    Add check system functionality + appropriate exception

--- convertool/utils.py
+++ convertool/utils.py
@@ -6,6 +6,16 @@
 # -----------------------------------------------------------------------------
 import os
 from typing import List
+import tqdm
+
+# -----------------------------------------------------------------------------
+# Classes
+# -----------------------------------------------------------------------------
+
+
+class WrongOSError(Exception):
+    """Implements an error to raise when the OS is not supported."""
+
 
 # -----------------------------------------------------------------------------
 # Function Definitions
@@ -32,8 +42,7 @@ def get_files(input_files: str) -> List[str]:
     # Traverse given path, collect results.
     # tqdm is used to show progress of os.walk
     if os.path.isdir(input_files):
-        for root, dirs, files in os.walk(input_files, topdown=True):
-            print(dirs)
+        for root, dirs, files in tqdm.tqdm(os.walk(input_files, topdown=True)):
             for file in files:
                 file_list.append(os.path.join(root, file))
     if os.path.isfile(input_files):
@@ -42,3 +51,22 @@ def get_files(input_files: str) -> List[str]:
                 file_list.append(line.strip())
 
     return file_list
+
+
+def check_system(system: str) -> None:
+    """Checks if a given system is supported. Raises WrongOSError if not.
+
+    Parameters
+    ----------
+    system : str
+        The system on which the script is running.
+
+    Raises
+    ------
+    WrongOSError
+        If the system is not Windows or Linux, convertool will not work.
+    """
+    if system not in ["Windows", "Linux"]:
+        raise WrongOSError(
+            f"Expected to run on Windows or Linux, got {system}."
+        )
