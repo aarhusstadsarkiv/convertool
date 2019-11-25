aaf96f3c72f10701ce5fb293df2bdd75e5b25667
Nina Jensen <jnik@aarhus.dk>
2019-11-25 17:04:17 +0100

    Update libreoffice tests

--- tests/test_libreoffice.py
+++ tests/test_libreoffice.py
@@ -1,13 +1,13 @@
 import os
 import platform
 import pytest
-from convertool.libreoffice import find_libre, convert_files
-from convertool.utils import (
-    get_files,
-    WrongOSError,
-    ConversionError,
+from convertool.libreoffice import (
+    find_libre,
+    convert_files,
     LibreError,
+    ConversionError,
 )
+from convertool.utils import get_files, WrongOSError
 
 
 class TestFindLibre:
@@ -39,28 +39,26 @@ class TestConvertFiles:
     out_path = os.path.join(test_path, "out")
     files = get_files(valid_path)
 
-    def test_with_valid_input(self):
+    def test_with_valid_input(self, caplog):
         convert_files(self.files, self.out_path)
         test_file = os.path.join(self.out_path, "test.pdf")
+        log_msg = [record.message for record in caplog.records]
+
+        # test.pdf is a file, and there should be no log message
         assert os.path.isfile(test_file)
+        assert not log_msg
         # Clean up
         os.remove(test_file)
 
     def test_with_invalid_command(self):
+        # This needs to be rewritten. Raises because ln(1)=0!!
         with pytest.raises(ConversionError):
             convert_files(self.files, self.out_path, libre="bogus")
-        log = os.path.join(self.out_path, "_convertool_log.txt")
-        with open(log) as file:
-            lines = file.readlines()
-            expected_msg = f"Conversion of {self.files[0]} failed with error"
-            assert expected_msg in lines[0]
 
-    def test_with_invalid_file(self):
+    def test_with_invalid_file(self, caplog):
         convert_files(["bogus"], self.out_path)
-        with open(os.path.join(self.out_path, "_convertool_log.txt")) as file:
-            lines = file.readlines()
-            expected_msg = "Conversion of bogus failed with error: "
-            expected_msg += "Error: source file could not be loaded\n"
-            assert expected_msg in lines
-        # Clean up
-        os.remove(os.path.join(self.out_path, "_convertool_log.txt"))
+        log_msg = [record.message for record in caplog.records]
+        assert (
+            "Conversion of bogus failed with error Error: source file"
+            in log_msg[0]
+        )
