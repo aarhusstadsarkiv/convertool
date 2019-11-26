3847e2a1aadf59c19a5693adf36ef4397a25aad5
Nina Jensen <jnik@aarhus.dk>
2019-11-26 15:50:17 +0100

    Update cli with pname option, change system call, update tests

--- convertool/cli.py
+++ convertool/cli.py
@@ -9,13 +9,9 @@ import platform
 from typing import List
 import click
 from convertool.libreoffice import convert_files as libre_convert
+from convertool.libreoffice import ConversionError
 from convertool.utils import get_files
 from convertool.utils import check_system, WrongOSError
-
-# -----------------------------------------------------------------------------
-# Constants
-# -----------------------------------------------------------------------------
-SYSTEM = platform.system()
 
 # -----------------------------------------------------------------------------
 # Function Definitions
@@ -29,15 +25,21 @@ SYSTEM = platform.system()
 @click.argument(
     "outdir", type=click.Path(exists=True, file_okay=False, resolve_path=True)
 )
+@click.option(
+    "--pname",
+    default=0,
+    show_default=True,
+    help="Number of parent directories to use for output name.",
+)
 @click.pass_context
-def cli(ctx: click.core.Context, files: str, outdir: str) -> None:
+def cli(ctx: click.core.Context, files: str, outdir: str, pname: int) -> None:
     """Convert files from a folder or a list. If FILES is a folder,
     convertool will convert every file in this folder and subfolders.
     If FILES is a file, convertool expects a text file with a list of
     files to convert. OUTDIR specifies the directory in which to output
     converted files. It must be an existing directory."""
     try:
-        check_system(SYSTEM)
+        check_system(platform.system())
     except WrongOSError as error:
         raise click.ClickException(str(error))
     else:
@@ -47,7 +49,7 @@ def cli(ctx: click.core.Context, files: str, outdir: str) -> None:
             raise click.ClickException(exit_msg)
 
         # Create object with state to pass around.
-        ctx.obj = {"file_list": file_list, "outdir": outdir}
+        ctx.obj = {"file_list": file_list, "outdir": outdir, "pname": pname}
 
 
 @cli.command()
@@ -55,6 +57,8 @@ def cli(ctx: click.core.Context, files: str, outdir: str) -> None:
 def libre(context: dict) -> None:
     """Convert files using LibreOffice."""
     try:
-        libre_convert(context["file_list"], context["outdir"])
-    except Exception as error:
+        libre_convert(
+            context["file_list"], context["outdir"], context["pname"]
+        )
+    except ConversionError as error:
         raise click.ClickException(str(error))
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,3 +1,4 @@
+import sys
 import os
 import pytest
 from click.testing import CliRunner
@@ -10,6 +11,52 @@ def cli_run():
 
 
 class TestCli:
+    script_path = os.path.dirname(os.path.realpath(__file__))
+    test_path = os.path.join(script_path, "test_data")
+    valid_path = os.path.join(test_path, "data")
+    out_path = os.path.join(test_path, "out")
+
     def test_with_valid_input(self, cli_run):
-        script_path = os.path.dirname(os.path.realpath(__file__))
-        valid_path = os.path.join(script_path, "test_data", "data")
+        with cli_run.isolated_filesystem():
+            args = [self.valid_path, self.out_path, "libre"]
+            result = cli_run.invoke(cli, args)
+            assert result.exit_code == 0
+
+    def test_with_invalid_files(self, cli_run):
+        with cli_run.isolated_filesystem():
+            args = ["bogus", self.out_path, "libre"]
+            result = cli_run.invoke(cli, args)
+            assert result.exit_code != 0
+            assert 'Error: Invalid value for "FILES"' in result.output
+            assert 'Path "bogus" does not exist.' in result.output
+
+    def test_with_invalid_outdir(self, cli_run):
+        with cli_run.isolated_filesystem():
+            args = [self.valid_path, "bogus", "libre"]
+            result = cli_run.invoke(cli, args)
+            assert result.exit_code != 0
+            assert 'Error: Invalid value for "OUTDIR"' in result.output
+            assert 'Directory "bogus" does not exist.' in result.output
+
+    def test_with_empty_filedir(self, cli_run):
+        empty_test = os.path.join(self.test_path, "test_empty.txt")
+        with cli_run.isolated_filesystem():
+            args = [empty_test, self.out_path, "libre"]
+            result = cli_run.invoke(cli, args)
+            assert result.exit_code != 0
+            assert f"{empty_test} is empty. Aborting" in result.output
+
+    def test_with_invalid_pname(self, cli_run):
+        with cli_run.isolated_filesystem():
+            args = [
+                f"--pname={sys.maxsize}",
+                self.valid_path,
+                self.out_path,
+                "libre",
+            ]
+            result = cli_run.invoke(cli, args)
+            assert result.exit_code != 0
+            assert (
+                f"Error: Parent index {sys.maxsize} out of range for"
+                in result.output
+            )
