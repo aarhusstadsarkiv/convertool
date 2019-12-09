80c28c936dc08d11d18bbaebaf45095a2f70872d
Nina Jensen <jnik@aarhus.dk>
2019-12-09 16:34:55 +0100

    Comment out ods.py as it is currently unused

--- convertool/ods.py
+++ convertool/ods.py
@@ -7,9 +7,9 @@
 # -----------------------------------------------------------------------------
 # import pyexcel_ods
 # import pyexcel
-import subprocess
-import pyautogui
-import time
+# import subprocess
+# import pyautogui
+# import time
 
 # -----------------------------------------------------------------------------
 # Function Definitions
@@ -40,19 +40,19 @@ import time
 #     for rec in recs:
 #         print(rec)
 #     print(recs["Daglig"])
-subprocess.Popen(
-    "libreoffice /home/jnik/Documents/test_data/test_3.ods", shell=True
-)
-time.sleep(3)
-pyautogui.press("f3")
-time.sleep(2)
-scale_coords = pyautogui.locateOnScreen(
-    "/home/jnik/Documents/scaling_mode.png"
-)
-print(scale_coords)
-time.sleep(1)
-scale_x = scale_coords.left + scale_coords.width + 10
-scale_y = pyautogui.center(scale_coords).y
-pyautogui.click(scale_x, scale_y)
-print(pyautogui.center(scale_coords))
-pyautogui.moveTo(scale_x, scale_y - 100)
+# subprocess.Popen(
+#     "libreoffice /home/jnik/Documents/test_data/test_3.ods", shell=True
+# )
+# time.sleep(3)
+# pyautogui.press("f3")
+# time.sleep(2)
+# scale_coords = pyautogui.locateOnScreen(
+#     "/home/jnik/Documents/scaling_mode.png"
+# )
+# print(scale_coords)
+# time.sleep(1)
+# scale_x = scale_coords.left + scale_coords.width + 10
+# scale_y = pyautogui.center(scale_coords).y
+# pyautogui.click(scale_x, scale_y)
+# print(pyautogui.center(scale_coords))
+# pyautogui.moveTo(scale_x, scale_y - 100)
