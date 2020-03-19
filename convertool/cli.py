966e79843f26365dbe3163cc0802aa6163f52dbc
Nina Jensen <jnik@aarhus.dk>
2020-03-19 16:01:25 +0100

    Import ACCEPTED_OUT from correct module in CLI

--- convertool/cli.py
+++ convertool/cli.py
@@ -10,8 +10,9 @@ import math
 from typing import List, Optional
 import click
 from click.core import Context as ClickContext
+from convertool.internals import ACCEPTED_OUT
 from convertool.convert import convert_files, SYMPHONY_IMPORTED, SYMPHONY_ERROR
-from convertool.utils import get_files, check_system, ACCEPTED_OUT
+from convertool.utils import get_files, check_system
 from convertool.exceptions import WrongOSError, ConversionError
 
 # -----------------------------------------------------------------------------
