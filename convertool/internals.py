60eb4465fa320402ebc49ecd7eeb1019b82d7aa2
Nina Jensen <jnik@aarhus.dk>
2020-03-19 15:23:59 +0100

    Add Pydantic model classes controlling file conversion

--- convertool/internals.py
+++ convertool/internals.py
@@ -4,8 +4,40 @@
 # -----------------------------------------------------------------------------
 # Imports
 # -----------------------------------------------------------------------------
+import math
+from pathlib import Path
+from typing import Optional, List, Any
+from pydantic import BaseModel, FilePath, DirectoryPath
+from convertool.utils import create_outdir
 
 # -----------------------------------------------------------------------------
 # Globals
 # -----------------------------------------------------------------------------
 ACCEPTED_OUT = ["pdf", "ods", "odt", "odp", "html", "png", "tiff"]
+
+# -----------------------------------------------------------------------------
+# Data Models
+# -----------------------------------------------------------------------------
+
+
+class File(BaseModel):
+    path: FilePath
+    outdir: DirectoryPath
+    parent_dirs: int = 0
+    encoding: Optional[int] = None
+
+    def __init__(self, **data: Any):
+        super().__init__(**data)
+        self.file_outdir: Path = create_outdir(
+            self.path, self.outdir, self.parent_dirs
+        )
+
+
+class FileConv(BaseModel):
+    files: List[File]
+    max_errs: Optional[int] = None
+
+    def __init__(self, **data: Any):
+        super().__init__(**data)
+        if not self.max_errs:
+            self.max_errs = int(math.sqrt(len(self.files)))
