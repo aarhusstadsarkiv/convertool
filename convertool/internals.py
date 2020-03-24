360fe2c3fdb5b49d8ca0d09c9404bfe6c4e01bcd
Nina Jensen <jnik@aarhus.dk>
2020-03-24 17:11:16 +0100

    Add Identification & FileInfo classes

--- convertool/internals.py
+++ convertool/internals.py
@@ -22,19 +22,51 @@ ACCEPTED_OUT = ["pdf", "ods", "odt", "odp", "html", "png", "tiff"]
 # -----------------------------------------------------------------------------
 
 
-class File(BaseModel):
-    path: Path
-    encoding: Optional[int]
-    parent_dirs: int = 0
+class Identification(BaseModel):
+    """File identification information model."""
 
-    def get_file_outdir(self, outdir: Path) -> Path:
-        return create_outdir(self.path, outdir, self.parent_dirs)
+    puid: Optional[str]
+    signame: Optional[str]
+    warning: Optional[str]
+
+
+class FileInfo(BaseModel):
+    """File information model."""
+
+    path: Path
+    name: str = ""
+    ext: str = ""
+    size: str = ""
+    checksum: Optional[str] = None
+    identification: Optional[Identification] = None
 
     @validator("path")
     def path_must_be_file(cls, path: Path) -> Path:
         if not path.is_file():
             raise ValueError("File does not exist.")
-        return path
+        return path.resolve()
+
+    @validator("name")
+    def name_overwrite(cls, name: str) -> str:
+        if name:
+            print(f"Warning! name={name} will be overwritten during init.")
+        return ""
+
+    def __init__(self, **data: Any):
+        super().__init__(**data)
+
+        # Resolve path, init fields
+        self.name = self.path.name
+        self.ext = self.path.suffix.lower()
+        self.size = size_fmt(self.path.stat().st_size)
+
+
+class File(FileInfo):
+    encoding: Optional[int]
+    parent_dirs: int = 0
+
+    def get_file_outdir(self, outdir: Path) -> Path:
+        return create_outdir(self.path, outdir, self.parent_dirs)
 
 
 class FileConv(BaseModel):
@@ -45,3 +77,28 @@ class FileConv(BaseModel):
         super().__init__(**data)
         if not self.max_errs:
             self.max_errs = int(math.sqrt(len(self.files)))
+
+
+# -----------------------------------------------------------------------------
+# Function Definitions
+# -----------------------------------------------------------------------------
+
+
+def size_fmt(size: float) -> str:
+    """Formats a file size in binary multiples to a human readable string.
+
+    Parameters
+    ----------
+    size : float
+        The file size in bytes.
+
+    Returns
+    -------
+    str
+        Human readable string representing size in binary multiples.
+    """
+    for unit in ["B", "KiB", "MiB", "GiB", "TiB"]:
+        if size < 1024.0:
+            break
+        size /= 1024.0
+    return f"{size:.1f} {unit}"
