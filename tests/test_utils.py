439f174186b62a3f55125bc9740ac4a5b6a08d9d
Nina Jensen <jnik@aarhus.dk>
2020-05-25 12:50:22 +0200

    Fix f-string error in test_utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -34,20 +34,20 @@ class TestCheckSystem:
 
 class TestRunProc:
     def test_successful_proc(self):
-        proc = Popen(f"exit 0", shell=True)
+        proc = Popen("exit 0", shell=True)
         run_proc(proc, timeout=30)
 
     def test_timeout(self):
-        proc = Popen(f"sleep 10", shell=True)
+        proc = Popen("sleep 10", shell=True)
         with pytest.raises(TimeoutExpired):
             run_proc(proc, timeout=0)
 
     def test_critical_error(self):
-        proc = Popen(f"exit 1", shell=True)
+        proc = Popen("exit 1", shell=True)
         with pytest.raises(ProcessError):
             run_proc(proc, timeout=30)
 
     def test_process_error(self):
-        proc = Popen(f">&2 echo error", shell=True, stderr=PIPE)
+        proc = Popen(">&2 echo error", shell=True, stderr=PIPE)
         with pytest.raises(ProcessError):
             run_proc(proc, timeout=30)
