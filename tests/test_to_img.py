4e46b3968b88da28304748eac8c9367e54cb7ee2
Matteo Campinoti <matteo.campinoti94@gmail.com>
2024-09-05 14:57:12 +0200

    tests.to_img:pdf_to_img - test ConverterPDFToImg

--- tests/test_to_img.py
+++ tests/test_to_img.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 from acacore.models.file import File
 
+from convertool.converters.converter_to_img import ConverterPDFToImg
 from convertool.converters.converter_to_img import ConverterTextToImg
 from convertool.converters.converter_to_img import ConverterToImg
 
@@ -17,6 +18,18 @@ def test_img_to_img(test_files: dict[str, Path], reference_files: dict[str, Path
         assert len(output_files) == 1
         assert output_files[0].name in reference_files
         assert reference_files[output_files[0].name].stat().st_size == output_files[0].stat().st_size
+
+
+# noinspection DuplicatedCode
+def test_pdf_to_img(test_files: dict[str, Path], reference_files: dict[str, Path], output_dir: Path):
+    file = File.from_file(test_files["pdf-to-img.pdf"], root=test_files["pdf-to-img.pdf"].parent)
+    converter = ConverterPDFToImg(file)
+
+    for output in converter.outputs:
+        print(output)
+        output_files = converter.convert(output_dir, output)
+        assert all(o.name in reference_files for o in output_files)
+        assert all(reference_files[o.name].stat().st_size == o.stat().st_size for o in output_files)
 
 
 # noinspection DuplicatedCode
