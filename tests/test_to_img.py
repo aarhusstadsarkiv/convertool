bb6382d77dc558cf6f8b09c1e9a9c523fe6e2269
Matteo Campinoti <matteo.campinoti94@gmail.com>
2024-09-05 14:56:41 +0200

    tests.to_img:img_to_img - test ConverterToImg

--- tests/test_to_img.py
+++ tests/test_to_img.py
@@ -3,8 +3,23 @@ from pathlib import Path
 from acacore.models.file import File
 
 from convertool.converters.converter_to_img import ConverterTextToImg
+from convertool.converters.converter_to_img import ConverterToImg
 
 
+# noinspection DuplicatedCode
+def test_img_to_img(test_files: dict[str, Path], reference_files: dict[str, Path], output_dir: Path):
+    file = File.from_file(test_files["img-to-img.webp"], root=test_files["img-to-img.webp"].parent)
+    converter = ConverterToImg(file)
+
+    for output in converter.outputs:
+        print(output)
+        output_files = converter.convert(output_dir, output)
+        assert len(output_files) == 1
+        assert output_files[0].name in reference_files
+        assert reference_files[output_files[0].name].stat().st_size == output_files[0].stat().st_size
+
+
+# noinspection DuplicatedCode
 def test_text_to_img(test_files: dict[str, Path], reference_files: dict[str, Path], output_dir: Path):
     file = File.from_file(test_files["text_to_img.txt"], root=test_files["text_to_img.txt"].parent)
     converter = ConverterTextToImg(file)
