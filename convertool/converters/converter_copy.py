0a00a2de3635aa82adfc81d4372f75555fe7bca3
Matteo Campinoti <matteo.campinoti94@gmail.com>
2024-09-27 09:57:37 +0200

    converters.converter_copy - add copy converter

---
+++ convertool/converters/converter_copy.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+from shutil import copy2
+from typing import ClassVar
+
+from .base import Converter
+
+
+class ConverterCopy(Converter):
+    tool_names: ClassVar[list[str]] = ["copy"]
+    outputs: ClassVar[list[str]] = ["copy"]
+
+    def output_file(self, output_dir: Path, output: str, *, append: bool = False) -> Path:
+        return output_dir / self.file.name
+
+    def convert(self, output_dir: Path, output: str, *, keep_relative_path: bool = True) -> list[Path]:
+        dest_dir: Path = self.output_dir(output_dir, keep_relative_path)
+        dest_file: Path = self.output_file(dest_dir, output)
+        copy2(self.file.get_absolute_path(), dest_file)
+        return [dest_file]
