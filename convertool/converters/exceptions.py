3f78bd23307d256ce1c3dbc4a8829be0e6c03453
Matteo Campinoti <matteo.campinoti94@gmail.com>
2024-09-27 09:49:08 +0200

    exceptions:ConvertError - add missing super call

--- convertool/converters/exceptions.py
+++ convertool/converters/exceptions.py
@@ -5,6 +5,7 @@ class ConvertError(Exception):
     def __init__(self, file: File, msg: str | BaseException | None = None) -> None:
         self.file: File = file
         self.msg: str | BaseException | None = msg
+        super().__init__(msg)
 
 
 class OutputDirError(ConvertError): ...
