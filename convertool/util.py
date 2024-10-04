12c18a9d7cdf159896a334adda2e6157803a547c
Matteo Campinoti <matteo.campinoti94@gmail.com>
2024-10-04 10:41:22 +0200

    util:run_process - add timeout parameter

--- convertool/util.py
+++ convertool/util.py
@@ -23,6 +23,7 @@ def run_process(
     cwd: Path | None = None,
     env: bool = True,
     capture_output: bool = True,
+    timeout: float | None = None,
 ) -> tuple[str, str]:
     """
     Run process and capture output.
@@ -33,7 +34,9 @@ def run_process(
     :param cwd: Optionally, the working directory to use.
     :param env: If ``True`` to use the system's env command (if available).
     :param capture_output: Whether to capture the output of ``subprocess.run``. Default: ``True``.
+    :param timeout: Optionally, a timeout.
     :raise CalledProcessError: If the process exists with a non-zero code.
+    :raise TimeoutExpired: If the process times out.
     :return: A tuple with the captured stdout and stderr outputs in string format.
     """
     try:
@@ -44,6 +47,7 @@ def run_process(
             capture_output=capture_output,
             encoding="utf-8",
             check=True,
+            timeout=timeout,
         )
         return process.stderr or "", process.stdout or ""
     except FileNotFoundError:
