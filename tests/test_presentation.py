0355177457a0b03d97aad273ce40c122eac155ac
Matteo Campinoti <matteo.campinoti94@gmail.com>
2024-10-04 17:07:23 +0200

    tests.presentation - add tests for presentation converter

---
+++ tests/test_presentation.py
@@ -0,0 +1,58 @@
+from os import environ
+from pathlib import Path
+
+from acacore.models.file import File
+from acacore.siegfried import Siegfried
+
+from convertool.converters.converter_presentation import ConverterPresentation
+
+
+# noinspection DuplicatedCode
+def test_presentation_to_ods(test_files: dict[str, Path], output_dir: Path):
+    for path in [f for n, f in test_files.items() if n.startswith("presentation.")]:
+        print(path.name)
+
+        file = File.from_file(path, root=path.parent)
+        converter = ConverterPresentation(file)
+        siegfried = Siegfried(environ["SIEGFRIED_PATH"], "default.sig", environ["SIEGFRIED_HOME"])
+
+        output_files = converter.convert(output_dir, "odt")
+        expected_output_file = file.relative_path.with_suffix(".odt")
+        assert len(output_files) == 1
+        assert expected_output_file.name in [f.name for f in output_files]
+        sf_match = siegfried.identify(output_dir / expected_output_file.name).files[0].best_match()
+        assert sf_match and sf_match.mime == "application/vnd.oasis.opendocument.presentation"
+
+
+# noinspection DuplicatedCode
+def test_presentation_to_pdf(test_files: dict[str, Path], output_dir: Path):
+    for path in [f for n, f in test_files.items() if n.startswith("presentation.")]:
+        print(path.name)
+
+        file = File.from_file(path, root=path.parent)
+        converter = ConverterPresentation(file)
+        siegfried = Siegfried(environ["SIEGFRIED_PATH"], "default.sig", environ["SIEGFRIED_HOME"])
+
+        output_files = converter.convert(output_dir, "pdf")
+        expected_output_file = file.relative_path.with_suffix(".pdf")
+        assert len(output_files) == 1
+        assert expected_output_file.name in [f.name for f in output_files]
+        sf_match = siegfried.identify(output_dir / expected_output_file.name).files[0].best_match()
+        assert sf_match and sf_match.mime == "application/pdf"
+
+
+# noinspection DuplicatedCode
+def test_presentation_to_html(test_files: dict[str, Path], output_dir: Path):
+    for path in [f for n, f in test_files.items() if n.startswith("presentation.")]:
+        print(path.name)
+
+        file = File.from_file(path, root=path.parent)
+        converter = ConverterPresentation(file)
+        siegfried = Siegfried(environ["SIEGFRIED_PATH"], "default.sig", environ["SIEGFRIED_HOME"])
+
+        output_files = converter.convert(output_dir, "html")
+        expected_output_file = file.relative_path.with_suffix(".html")
+        assert len(output_files) == 1
+        assert expected_output_file.name in [f.name for f in output_files]
+        sf_match = siegfried.identify(output_dir / expected_output_file.name).files[0].best_match()
+        assert sf_match and sf_match.mime == "text/html"
