4f8cbbba25d175efb1e6246cda8ae7764c714032
Matteo Campinoti <matteo.campinoti94@gmail.com>
2024-10-07 12:59:25 +0200

    tests - add tests for audio converter

---
+++ tests/test_audio.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+from acacore.models.file import File
+from acacore.siegfried import Siegfried
+
+from convertool.converters import ConverterAudio
+
+
+def test_audio_to_mp3(test_files: dict[str, Path], output_dir: Path, siegfried: Siegfried):
+    file = File.from_file(test_files["audio.m4a"], root=test_files["audio.m4a"].parent)
+    converter = ConverterAudio(file)
+
+    output_files = converter.convert(output_dir, "mp3")
+    assert len(output_files) == 1
+    assert siegfried.identify(output_files[0]).files[0].best_match().mime == "audio/mp3"
+
+
+def test_audio_to_wav(test_files: dict[str, Path], output_dir: Path, siegfried: Siegfried):
+    file = File.from_file(test_files["audio.m4a"], root=test_files["audio.m4a"].parent)
+    converter = ConverterAudio(file)
+
+    output_files = converter.convert(output_dir, "wav")
+    assert len(output_files) == 1
+    assert siegfried.identify(output_files[0]).files[0].best_match().mime in ("audio/wav", "audio/x-wav")
