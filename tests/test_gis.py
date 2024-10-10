137cd245d2aa5a906cd84d0d630ac38cf3805839
Matteo Campinoti <matteo.campinoti94@gmail.com>
2024-10-10 15:31:15 +0200

    tests.gis - fix incorrect import

--- tests/test_gis.py
+++ tests/test_gis.py
@@ -1,15 +1,14 @@
 from pathlib import Path
-from wsgiref.validate import assert_
 
 from acacore.models.file import File
 
-from convertool.converters import ConverterMSG
+from convertool.converters import ConverterGIS
 
 
 # noinspection DuplicatedCode
 def test_gis_to_gml(test_files: dict[str, Path], reference_files: dict[str, Path], output_dir: Path):
     file = File.from_file(test_files["gis.tab"], root=test_files["gis.tab"].parent)
-    converter = ConverterMSG(file)
+    converter = ConverterGIS(file)
 
     output_files = converter.convert(output_dir, "gml")
     assert len(output_files) == 2
