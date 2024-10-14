0c49d2bed28c859a592a5d136405bcfafb7b6bba
Matteo Campinoti <matteo.campinoti94@gmail.com>
2024-10-14 12:55:55 +0200

    converters.cad - use temporary dir to get the actual converted file name

--- convertool/converters/converter_cad.py
+++ convertool/converters/converter_cad.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 from typing import ClassVar
 
+from acacore.utils.functions import rm_tree
+
 from .base import ConverterABC
 
 
@@ -14,8 +16,12 @@ class ConverterCAD(ConverterABC):
     def convert(self, output_dir: Path, output: str, *, keep_relative_path: bool = True) -> list[Path]:
         output = self.output(output)
         dest_dir: Path = self.output_dir(output_dir, keep_relative_path)
-        dest_file: Path = self.output_file(dest_dir, output)
+        dest_dir_tmp: Path = dest_dir.joinpath(f"_tmp_{self.file.uuid}")
+        rm_tree(dest_dir_tmp)
+        dest_dir_tmp.mkdir(parents=True, exist_ok=True)
 
-        self.run_process("ABViewer", "/c", output, f"dir={output_dir}", self.file.get_absolute_path())
-
-        return [dest_file]
+        try:
+            self.run_process("ABViewer", "/c", output, f"dir={dest_dir_tmp}", self.file.get_absolute_path())
+            return [f.replace(dest_dir / f.name) for f in dest_dir_tmp.iterdir() if f.is_file()]
+        finally:
+            rm_tree(dest_dir_tmp)
