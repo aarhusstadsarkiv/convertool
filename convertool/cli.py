1c2119ccb1b944ce00238d55a075ce907d24a4f8
Matteo Campinoti <matteo.campinoti94@gmail.com>
2024-10-31 13:29:04 +0100

    Revert "cli:digiarch - log output file names"
    This reverts commit 3c8ed6b4b89dfaf425cd1b0ff1c6db1cde2947f2.

--- convertool/cli.py
+++ convertool/cli.py
@@ -244,14 +244,7 @@ def digiarch(
                     file.processed = True
                     file.processed_names = [d.name for d in dests]
                     database.files.update(file)
-                    database.history.insert(
-                        HistoryEntry.command_history(
-                            ctx,
-                            "converted",
-                            file.uuid,
-                            [tool, output, file.processed_names],
-                        )
-                    )
+                    database.history.insert(HistoryEntry.command_history(ctx, "converted", file.uuid, [tool, output]))
 
         end_program(ctx, database, exception, dry_run, log_file, log_stdout)
 
