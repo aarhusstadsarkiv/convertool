7e3f13c580d7b9719eed89a065f0d23aae0d75d7
Matteo Campinoti <matteo.campinoti94@gmail.com>
2024-11-08 14:26:19 +0100

    converters.video - add h264-mpg output to use H.264 encode and .mpg extension

--- convertool/converters/converter_video.py
+++ convertool/converters/converter_video.py
@@ -11,6 +11,7 @@ class ConverterVideo(ConverterABC):
     outputs: ClassVar[list[str]] = [
         "mpeg2",
         "h264",
+        "h264-mpg",
         "h265",
     ]
     process_timeout: ClassVar[float] = 7200
@@ -19,6 +20,7 @@ class ConverterVideo(ConverterABC):
     def convert(self, output_dir: Path, output: str, *, keep_relative_path: bool = True) -> list[Path]:
         output = self.output(output)
         dest_dir: Path = self.output_dir(output_dir, keep_relative_path=keep_relative_path)
+        final_extension: str = ""
         arguments: list[str] = []
 
         if output == "mpeg2":
@@ -26,6 +28,10 @@ class ConverterVideo(ConverterABC):
             arguments.extend(["-c:v", "mpeg2video", "-c:a", "mp3"])
         elif output == "h264":
             output = "mp4"
+            arguments.extend(["-c:v", "libx264", "-c:a", "aac"])
+        elif output == "h264-mpg":
+            output = "mp4"
+            final_extension = "mpg"
             arguments.extend(["-c:v", "libx264", "-c:a", "aac"])
         elif output == "h265":
             output = "mp4"
@@ -47,6 +53,9 @@ class ConverterVideo(ConverterABC):
                 dest_file.name,
                 cwd=tmp_dir,
             )
+            if final_extension:
+                tmp_file = tmp_dir.joinpath(dest_file.name)
+                dest_file = dest_file.with_name(tmp_file.replace(self.output_file(tmp_dir, final_extension)).name)
             dest_dir.mkdir(parents=True, exist_ok=True)
             tmp_dir.joinpath(dest_file.name).replace(dest_file)
 
