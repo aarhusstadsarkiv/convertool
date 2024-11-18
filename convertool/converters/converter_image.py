b20660573684c0a089d823404324d0e48f5b619b
Matteo Campinoti <matteo.campinoti94@gmail.com>
2024-11-18 12:26:56 +0100

    converters.image:ConverterPDFToImage - add "pdf" to tool names

--- convertool/converters/converter_image.py
+++ convertool/converters/converter_image.py
@@ -44,7 +44,10 @@ class ConverterImage(ConverterABC):
 
 
 class ConverterPDFToImage(ConverterImage):
-    tool_names: ClassVar[list[str]] = ["pdf-to-image"]
+    tool_names: ClassVar[list[str]] = [
+        "pdf",
+        "pdf-to-image",
+    ]
 
     def convert(self, output_dir: Path, output: str, *, keep_relative_path: bool = True) -> list[Path]:
         output = self.output(output)
