2812c7689edcd493126a3f9f4d811a214ad21963
Matteo Campinoti <matteo.campinoti94@gmail.com>
2024-11-18 20:12:39 +0100

    tests.image - remove reference files check

--- tests/test_image.py
+++ tests/test_image.py
@@ -20,12 +20,7 @@ MIMETYPES = {
 
 
 # noinspection DuplicatedCode
-def test_img_to_img(
-    test_files: dict[str, Path],
-    reference_files: dict[str, Path],
-    output_dir: Path,
-    siegfried: Siegfried,
-):
+def test_img_to_img(test_files: dict[str, Path], output_dir: Path, siegfried: Siegfried):
     file = File.from_file(test_files["img-to-img.webp"], root=test_files["img-to-img.webp"].parent)
     converter = ConverterImage(file)
 
@@ -33,17 +28,11 @@ def test_img_to_img(
         print(output)
         output_files = converter.convert(output_dir, output)
         assert len(output_files) == 1
-        assert output_files[0].name in reference_files
         assert siegfried.identify(output_files[0]).files[0].best_match().mime == MIMETYPES[output]
 
 
 # noinspection DuplicatedCode
-def test_pdf_to_img(
-    test_files: dict[str, Path],
-    reference_files: dict[str, Path],
-    output_dir: Path,
-    siegfried: Siegfried,
-):
+def test_pdf_to_img(test_files: dict[str, Path], output_dir: Path, siegfried: Siegfried):
     file = File.from_file(test_files["pdf-to-img.pdf"], root=test_files["pdf-to-img.pdf"].parent)
     converter = ConverterPDFToImage(file)
 
@@ -51,17 +40,11 @@ def test_pdf_to_img(
         print(output)
         output_files = converter.convert(output_dir, output)
         assert len(output_files) >= 1
-        assert all(o.name in reference_files for o in output_files)
         assert all(sf.best_match().mime == MIMETYPES[output] for sf in siegfried.identify(*output_files).files)
 
 
 # noinspection DuplicatedCode
-def test_text_to_img(
-    test_files: dict[str, Path],
-    reference_files: dict[str, Path],
-    output_dir: Path,
-    siegfried: Siegfried,
-):
+def test_text_to_img(test_files: dict[str, Path], output_dir: Path, siegfried: Siegfried):
     file = File.from_file(test_files["text_to_img.txt"], root=test_files["text_to_img.txt"].parent)
     converter = ConverterTextToImage(file)
 
@@ -69,16 +52,10 @@ def test_text_to_img(
         print(output)
         output_files = converter.convert(output_dir, output)
         assert len(output_files) == 1
-        assert output_files[0].name in reference_files
         assert all(sf.best_match().mime == MIMETYPES[output] for sf in siegfried.identify(*output_files).files)
 
 
-def test_doc_to_img(
-    test_files: dict[str, Path],
-    reference_files: dict[str, Path],
-    output_dir: Path,
-    siegfried: Siegfried,
-):
+def test_doc_to_img(test_files: dict[str, Path], output_dir: Path, siegfried: Siegfried):
     file = File.from_file(test_files["document.docx"], root=test_files["document.docx"].parent)
     converter = ConverterDocumentToImage(file)
 
