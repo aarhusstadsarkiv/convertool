ae2127c1628f636ebfeedd10c58cbe2c633cf1c3
Matteo Campinoti <matteo.campinoti94@gmail.com>
2024-11-27 15:40:49 +0100

    tests.digiarch - add tests for master:access and master:statutory targets

--- tests/test_digiarch.py
+++ tests/test_digiarch.py
@@ -42,3 +42,71 @@ def test_digiarch_original_master(avid_dir_copy: Path):
             else:
                 assert not output_files
                 assert not file.processed
+
+
+def test_digiarch_master_access(avid_dir_copy: Path):
+    avid = AVID(avid_dir_copy)
+
+    with FilesDB(avid.database_path) as db:
+        db.access_files.delete("uuid is not null")
+        db.statutory_files.delete("uuid is not null")
+        # noinspection SqlWithoutWhere
+        db.execute(f"update {db.master_files.name} set processed = false")
+        db.commit()
+        rm_tree(avid.dirs.access_documents)
+        rm_tree(avid.dirs.documents)
+
+    app.main(["digiarch", str(avid.path), "master:access"], standalone_mode=False)
+
+    with FilesDB(avid.database_path) as db:
+        for file in db.master_files.select():
+            output_files = db.access_files.select({"original_uuid": str(file.uuid)}).fetchall()
+            event = db.log.select(
+                "file_uuid = ? and operation = 'convertool.digiarch:converted'",
+                [str(file.uuid)],
+            ).fetchone()
+            if event and event.data == ["template", "temporary-file"]:
+                assert not output_files
+                assert file.processed
+            elif event:
+                output_files = db.access_files.select({"original_uuid": str(file.uuid)}).fetchall()
+                assert len(output_files) >= 1
+                assert all(f.get_absolute_path(avid.path).is_file() for f in output_files)
+                assert file.processed
+            else:
+                assert not output_files
+                assert not file.processed
+
+
+def test_digiarch_master_statutory(avid_dir_copy: Path):
+    avid = AVID(avid_dir_copy)
+
+    with FilesDB(avid.database_path) as db:
+        db.access_files.delete("uuid is not null")
+        db.statutory_files.delete("uuid is not null")
+        # noinspection SqlWithoutWhere
+        db.execute(f"update {db.master_files.name} set processed = false")
+        db.commit()
+        rm_tree(avid.dirs.access_documents)
+        rm_tree(avid.dirs.documents)
+
+    app.main(["digiarch", str(avid.path), "master:statutory"], standalone_mode=False)
+
+    with FilesDB(avid.database_path) as db:
+        for file in db.master_files.select():
+            output_files = db.statutory_files.select({"original_uuid": str(file.uuid)}).fetchall()
+            event = db.log.select(
+                "file_uuid = ? and operation = 'convertool.digiarch:converted'",
+                [str(file.uuid)],
+            ).fetchone()
+            if event and event.data == ["template", "temporary-file"]:
+                assert not output_files
+                assert file.processed
+            elif event:
+                output_files = db.access_files.select({"original_uuid": str(file.uuid)}).fetchall()
+                assert len(output_files) >= 1
+                assert all(f.get_absolute_path(avid.path).is_file() for f in output_files)
+                assert file.processed
+            else:
+                assert not output_files
+                assert not file.processed
