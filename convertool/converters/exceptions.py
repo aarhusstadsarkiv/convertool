f7d31b5d6ad411161b6e4aed29a6464a4b4da997
Matteo Campinoti <matteo.campinoti94@gmail.com>
2025-01-09 09:30:41 +0100

    converters.exceptions:ConvertError - add variable "process" to store a CompletedProcess or TimeoutExpired object

--- convertool/converters/exceptions.py
+++ convertool/converters/exceptions.py
@@ -1,3 +1,6 @@
+from subprocess import CalledProcessError
+from subprocess import TimeoutExpired
+
 from acacore.models.file import BaseFile
 
 
@@ -8,9 +11,15 @@ class UnsupportedPlatform(Exception): ...
 
 
 class ConvertError(Exception):
-    def __init__(self, file: BaseFile, msg: str | BaseException | None = None) -> None:
+    def __init__(
+        self,
+        file: BaseFile,
+        msg: str | BaseException | None = None,
+        process: CalledProcessError | TimeoutExpired | None = None,
+    ) -> None:
         self.file: BaseFile = file
         self.msg: str | BaseException | None = msg
+        self.process: CalledProcessError | TimeoutExpired | None = process
         super().__init__(msg)
 
 
