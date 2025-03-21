88c5d28b7d283acd185aef1d3f45101ae82f139b
Matteo Campinoti <matteo.campinoti94@gmail.com>
2025-03-21 15:25:51 +0100

    tests.xsl - add tests for MedCom converters

--- tests/test_xsl.py
+++ tests/test_xsl.py
@@ -3,6 +3,9 @@ from pathlib import Path
 from acacore.models.file import BaseFile
 from acacore.siegfried import Siegfried
 
+from convertool.converters import ConverterMedCom
+from convertool.converters import ConverterMedComToImage
+from convertool.converters import ConverterMedComToPDF
 from convertool.converters import ConverterXSL
 from convertool.converters import ConverterXSLToImage
 from convertool.converters import ConverterXSLToPDF
@@ -35,6 +38,39 @@ def test_xml_to_pdf(test_files: dict[str, Path], output_dir: Path, siegfried: Si
 def test_xml_to_image(test_files: dict[str, Path], output_dir: Path, siegfried: Siegfried):
     file = BaseFile.from_file(test_files["medcom.xml"], root=test_files["medcom.xml"].parent)
     converter = ConverterXSLToImage(file)
+
+    for output in converter.outputs:
+        output_files = converter.convert(output_dir, output)
+        assert len(output_files) > 1
+        assert all(f.is_file() for f in output_files)
+        assert all(f.best_match().mime == MIMETYPES[output] for f in siegfried.identify(*output_files).files)
+
+
+def test_medcom_to_html(test_files: dict[str, Path], reference_files: dict[str, Path], output_dir: Path):
+    file = BaseFile.from_file(test_files["medcom.xml"], root=test_files["medcom.xml"].parent)
+    converter = ConverterMedCom(file)
+
+    output_files = converter.convert(output_dir, "html")
+    assert len(output_files) == 1
+    assert output_files[0].is_file()
+    assert output_files[0].name in reference_files
+    assert output_files[0].read_text() == reference_files[output_files[0].name].read_text()
+
+
+def test_medcom_to_pdf(test_files: dict[str, Path], output_dir: Path, siegfried: Siegfried):
+    file = BaseFile.from_file(test_files["medcom.xml"], root=test_files["medcom.xml"].parent)
+    converter = ConverterMedComToPDF(file)
+
+    output_files = converter.convert(output_dir, "pdf")
+    assert len(output_files) == 1
+    assert output_files[0].is_file()
+    match = siegfried.identify(output_files[0]).files[0]
+    assert match.best_match().mime == "application/pdf"
+
+
+def test_medcom_to_image(test_files: dict[str, Path], output_dir: Path, siegfried: Siegfried):
+    file = BaseFile.from_file(test_files["medcom.xml"], root=test_files["medcom.xml"].parent)
+    converter = ConverterMedComToImage(file)
 
     for output in converter.outputs:
         output_files = converter.convert(output_dir, output)
