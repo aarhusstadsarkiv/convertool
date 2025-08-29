fa51bb3ae7c607c894fdd694a6d57efca678eec1
Matteo Campinoti <matteo.campinoti94@gmail.com>
2025-08-29 14:09:47 +0200

    converters.mdi - add converter for MDI files to PDF

--- src/convertool/converters/converter_mdi.py
+++ src/convertool/converters/converter_mdi.py
@@ -3,7 +3,12 @@ from typing import ClassVar
 
 from convertool.util import TempDir
 
+from .base import _dummy_base_file
+from .base import _shared_dependencies
+from .base import _shared_platforms
+from .base import _shared_process_timeout
 from .base import ConverterABC
+from .converter_image import ConverterImage
 from .exceptions import ConvertError
 
 
@@ -40,5 +45,37 @@ class ConverterMDI(ConverterABC):
             if tmp_file.is_file():
                 dest_dir.mkdir(parents=True, exist_ok=True)
                 return [tmp_file.replace(dest_file)]
+
+        raise ConvertError(self.file, "Could not convert file.")
+
+
+class ConverterMDIToPDF(ConverterMDI):
+    outputs: ClassVar[list[str]] = ["pdf"]
+    process_timeout: ClassVar[float] = _shared_process_timeout(ConverterMDI, ConverterImage)
+    platforms: ClassVar[list[str]] = _shared_platforms(ConverterMDI, ConverterImage)
+    dependencies: ClassVar[dict[str, list[str]]] = _shared_dependencies(ConverterMDI, ConverterImage)
+
+    def output(self, output: str) -> str:
+        if output == "tiff":
+            output = "tif"
+        return super().output(output)
+
+    def convert(self, output_dir: Path, output: str, *, keep_relative_path: bool = True) -> list[Path]:
+        output = self.output(output)
+        dest_dir: Path = self.output_dir(output_dir, keep_relative_path=keep_relative_path)
+        dest_file: Path = self.output_file(dest_dir, output)
+
+        with TempDir(output_dir) as tmp_dir:
+            tiff: Path = super().convert(tmp_dir, "tif", keep_relative_path=False)[0]
+            pdfs: list[Path] = ConverterImage(
+                _dummy_base_file(tiff, tmp_dir),
+                None,
+                tmp_dir,
+                capture_output=self.capture_output,
+            ).convert(tmp_dir, output, keep_relative_path=False)
+
+            if pdfs:
+                dest_file.parent.mkdir(parents=True, exist_ok=True)
+                return [pdfs[0].replace(dest_file)]
 
         raise ConvertError(self.file, "Could not convert file.")
