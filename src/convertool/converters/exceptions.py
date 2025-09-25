3eeee0603ffddfd39f4d4fbd8536d7c8114b8195
Matteo Campinoti <matteo.campinoti94@gmail.com>
2025-09-25 12:36:09 +0200

    converters.exception:ConverterNotFound - add error for when a converter cannot be found

--- src/convertool/converters/exceptions.py
+++ src/convertool/converters/exceptions.py
@@ -1,5 +1,6 @@
 from subprocess import CalledProcessError
 from subprocess import TimeoutExpired
+from typing import Any
 
 from acacore.models.file import BaseFile
 
@@ -8,6 +9,17 @@ class MissingDependency(Exception): ...
 
 
 class UnsupportedPlatform(Exception): ...
+
+
+class ConverterNotFound(Exception):
+    def __init__(self, tool: str | None, output: str | None, *args: Any):
+        self.tool: str | None = tool
+        self.output: str | None = output
+        super().__init__(*args)
+
+    @property
+    def tool_output(self) -> str | None:
+        return f"{self.tool}:{self.output}" if self.tool or self.output else None
 
 
 class BadOption(Exception): ...
