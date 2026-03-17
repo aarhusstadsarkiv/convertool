7eb337ece35a6c51aab8669fbf794ab8a07b14a2
Matteo Campinoti <matteo.campinoti94@gmail.com>
2026-03-17 10:41:52 +0100

    tests.sas - add sas to ods test

--- tests/test_sas.py
+++ tests/test_sas.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 from convertool.converters import ConverterSAS
 from convertool.converters.base import dummy_base_file
+from convertool.converters.converter_sas import ConverterSASSpreadsheet
 
 
 def test_sas_to_csv(test_files: dict[str, Path], output_dir: Path):
@@ -22,3 +23,13 @@ def test_sas_to_tsv(test_files: dict[str, Path], output_dir: Path):
         output_files = converter.convert(output_dir, "tsv")
         assert len(output_files) == 1
         assert output_files[0].suffix == ".tsv"
+
+
+def test_sas_to_ods(test_files: dict[str, Path], output_dir: Path):
+    for path in [f for n, f in test_files.items() if n.endswith(".sas7bdat")]:
+        file = dummy_base_file(path, path.parent)
+        converter = ConverterSASSpreadsheet(file)
+
+        output_files = converter.convert(output_dir, "ods")
+        assert len(output_files) == 1
+        assert output_files[0].suffix == ".ods"
