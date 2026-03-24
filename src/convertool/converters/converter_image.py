9b89a8c2a41270a07178ef84ee6f93983206bfc2
Matteo Campinoti <matteo.campinoti94@gmail.com>
2026-03-24 11:39:43 +0100

    Revert "Ignore tag 32934 for '.tif'"
    This reverts commit d52d44f9

--- src/convertool/converters/converter_image.py
+++ src/convertool/converters/converter_image.py
@@ -59,7 +59,7 @@ class ConverterImage(ConverterABC):
             filename = filename.with_name(filename.name + "[0]")
             args.extend(("-background", "none", "-flatten"))
         if output == "tif":
-            args.extend(("-compress", "LZW", "-depth", "16", "-define", "tiff:ignore-tags=32934"))
+            args.extend(("-compress", "LZW", "-depth", "16"))
 
         with TempDir(output_dir) as tmp_dir:
             self.run_process(
@@ -85,7 +85,7 @@ class ConverterPDFToImage(ConverterImage):
         args: list[str] = []
 
         if output in ("tif", "tiff"):
-            args = ["-compress", "LZW", "-depth", "16", "-define", "tiff:ignore-tags=32934"]
+            args = ["-compress", "LZW", "-depth", "16"]
 
         density, _ = self.image_dpi(self.file.get_absolute_path())
         density *= 2
@@ -144,8 +144,6 @@ class ConverterPDFLargeToImage(ConverterImage):
                 "LZW",
                 "-depth",
                 "16",
-                "-define",
-                "tiff:ignore-tags=32934",
                 f"{tmp_dir.name}-*.jpg",
                 dest_file.name,
                 cwd=tmp_dir,
