7dfb320ff291f52b4ba6dafc206bd49f31568483
Matteo Campinoti <matteo.campinoti94@gmail.com>
2026-04-15 10:54:02 +0200

    tests.pdf - remove test for `ConverterPDFToJPEG2000`
    Does not exist anymore

--- tests/test_pdf.py
+++ tests/test_pdf.py
@@ -5,7 +5,6 @@ from acacore.siegfried import Siegfried
 from convertool.converters.base import dummy_base_file
 from convertool.converters.converter_pdf import ConverterPDF
 from convertool.converters.converter_pdf import ConverterPDFToImage
-from convertool.converters.converter_pdf import ConverterPDFToJPEG2000
 
 from .test_image import MIMETYPES
 
@@ -29,18 +28,6 @@ def test_pdf_to_pdfa(test_files: dict[str, Path], output_dir: Path, siegfried: S
 def test_pdf_to_img(test_files: dict[str, Path], output_dir: Path, siegfried: Siegfried):
     file = dummy_base_file(test_files["pdf-to-img.pdf"], test_files["pdf-to-img.pdf"].parent)
     converter = ConverterPDFToImage(file)
-
-    for output in converter.outputs:
-        print(output)
-        output_files = converter.convert(output_dir, output)
-        assert len(output_files) >= 1
-        assert all(sf.best_match().mime == MIMETYPES[output] for sf in siegfried.identify(*output_files).files)
-
-
-# noinspection DuplicatedCode
-def test_pdf_to_jp2(test_files: dict[str, Path], output_dir: Path, siegfried: Siegfried):
-    file = dummy_base_file(test_files["pdf-to-img.pdf"], test_files["pdf-to-img.pdf"].parent)
-    converter = ConverterPDFToJPEG2000(file)
 
     for output in converter.outputs:
         print(output)
