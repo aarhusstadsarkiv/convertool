3e9c726e5f653f459cdb25d475810fe5dc8c5bb4
Matteo Campinoti <matteo.campinoti94@gmail.com>
2026-04-16 11:10:36 +0200

    installers - do not use sudo
    Assume it is called with sudo

--- installers.sh
+++ installers.sh
@@ -1,13 +1,13 @@
-sudo apt update
+apt update
 
 # Base dependencies
-sudo apt install -y \
+apt install -y \
     vim \
     curl \
     wget \
     git \
     cmake \
-    sudo apt-transport-https \
+    apt-transport-https \
     ca-certificates \
     libc6-i386 \
     libc6-x32 \
@@ -17,10 +17,10 @@ sudo apt install -y \
 mkdir -p ~/.local/bin
 
 # Install GDAL
-sudo apt install -y libproj-dev gdal-bin
+apt install -y libproj-dev gdal-bin
 
 # Install Imagemagick
-sudo apt install -y imagemagick
+apt install -y imagemagick
 
 # Install nconvert
 curl -L -o nconvert.tgz 'https://download.xnview.com/NConvert-linux64.tgz'
@@ -29,23 +29,23 @@ mv NConvert/* ~/.local/bin
 rm -rf nconvert.tgz NConvert/
 
 # Install vipps
-sudo apt install -y libvips-tools
+apt install -y libvips-tools
 
 # Install LibreOffice
 curl -o libreoffice.tar.gz 'https://downloadarchive.documentfoundation.org/libreoffice/old/24.8.7.2/deb/x86_64/LibreOffice_24.8.7.2_Linux_x86-64_deb.tar.gz'
 tar zxvf libreoffice.tar.gz
 dpkg -i LibreOffice_24.8.7.2_Linux_x86-64_deb/DEBS/*.deb
-sudo ln -s /opt/libreoffice24.8/program/soffice /usr/bin/libreoffice
+ln -s /opt/libreoffice24.8/program/soffice /usr/bin/libreoffice
 rm -rf libreoffice LibreOffice_24.8.7.2_Linux_x86-64_deb/
 
 # Install GhostScript
-sudo apt install -y ghostscript
+apt install -y ghostscript
 
 # Install ffmpeg
-sudo apt install -y ffmpeg
+apt install -y ffmpeg
 
 # Install xmlstarlet
-sudo apt install -y xmlstarlet
+apt install -y xmlstarlet
 
 # Install chrome
-sudo apt install -y chromium
+apt install -y chromium
