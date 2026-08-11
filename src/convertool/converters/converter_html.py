3e3893f997b6a79f48a3e95c762ccce159a36787
Matteo Campinoti <matteo.campinoti94@gmail.com>
2026-08-11 14:59:20 +0200

    converters.html - use weasyprint instead of chromium

--- src/convertool/converters/converter_html.py
+++ src/convertool/converters/converter_html.py
@@ -1,6 +1,7 @@
 from pathlib import Path
-from subprocess import CalledProcessError
 from typing import ClassVar
+
+from weasyprint import HTML
 
 from convertool.util import TempDir
 
@@ -10,14 +11,11 @@ from .base import _shared_process_timeout
 from .base import ConverterABC
 from .base import dummy_base_file
 from .converter_pdf import ConverterPDFToImage
-from .exceptions import ConvertError
 
 
 class ConverterHTML(ConverterABC):
-    tool_names: ClassVar[list[str]] = ["html", "browser"]
+    tool_names: ClassVar[list[str]] = ["html"]
     outputs: ClassVar[list[str]] = ["pdf"]
-    dependencies: ClassVar[dict[str, list[str]]] = {"chromium": ["chromium", "chromium-browser", "google-chrome"]}
-    process_timeout: ClassVar[float] = 60
 
     def convert(self, output_dir: Path, output: str, *, keep_relative_path: bool = True) -> list[Path]:
         output = self.output(output)
@@ -25,37 +23,21 @@ class ConverterHTML(ConverterABC):
         dest_file: Path = self.output_file(dest_dir, output)
 
         with TempDir(output_dir) as tmp_dir:
-            tmp_file = tmp_dir.joinpath("output.pdf")
-            [_, _, process_result] = self.run_process(
-                self.dependencies["chromium"][0],
-                "--headless",
-                "--no-sandbox",
-                f"--print-to-pdf={tmp_file}",
-                "--no-pdf-header-footer",
-                self.file.get_absolute_path(),
-                cwd=tmp_dir,
+            html = HTML(
+                self.file.get_absolute_path().read_text(self.file.encoding["encoding"] if self.file.encoding else None)
             )
 
-            if not tmp_file.is_file():
-                raise ConvertError(
-                    self.file,
-                    "Output file not found.",
-                    CalledProcessError(
-                        process_result.returncode,
-                        process_result.args,
-                        process_result.stdout,
-                        process_result.stderr,
-                    ),
-                )
+            html.write_pdf(tmp_file := tmp_dir.joinpath(dest_file.name))
 
             dest_dir.mkdir(parents=True, exist_ok=True)
+
             tmp_file.replace(dest_file)
 
         return [dest_file]
 
 
 class ConverterHTMLToImage(ConverterABC):
-    tool_names: ClassVar[list[str]] = ["html", "browser"]
+    tool_names: ClassVar[list[str]] = ["html"]
     outputs: ClassVar[list[str]] = ConverterPDFToImage.outputs
     platforms: ClassVar[list[str] | None] = _shared_platforms(ConverterHTML, ConverterPDFToImage)
     dependencies: ClassVar[dict[str, list[str]] | None] = _shared_dependencies(ConverterHTML, ConverterPDFToImage)
