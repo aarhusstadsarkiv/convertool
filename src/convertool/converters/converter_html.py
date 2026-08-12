fb9f60a5b56bda8252cb1eb552902eab533de6bb
Matteo Campinoti <matteo.campinoti94@gmail.com>
2026-08-12 10:14:01 +0200

    converters.html - use weasyprint

--- src/convertool/converters/converter_html.py
+++ src/convertool/converters/converter_html.py
@@ -1,24 +1,22 @@
 from pathlib import Path
-from subprocess import CalledProcessError
 from typing import ClassVar
+
+from weasyprint import HTML
 
 from convertool.util import TempDir
 
 from .base import ConverterABC
-from .exceptions import ConvertError
 
 
 class HTMLConverter(ConverterABC):
     name: ClassVar[str] = "html"
     outputs: ClassVar[list[str]] = ["pdf"]
-    dependencies: ClassVar[dict[str, list[str]]] = {"chromium": ["chromium", "chromium-browser", "google-chrome"]}
-    process_timeout: ClassVar[int] = 60
 
     @classmethod
-    def output_name(cls, output: str) -> str:
+    def output_name(cls, output: str) -> str:  # noqa: ARG003
         return "pdf"
 
-    def output_extension(self, output: str) -> str:
+    def output_extension(self, output: str) -> str:  # noqa: ARG002
         return ".pdf"
 
     def convert(self, output_dir: Path, output: str, *, keep_relative_path: bool = True) -> list[Path]:
@@ -27,29 +25,7 @@ class HTMLConverter(ConverterABC):
         dest_file: Path = dest_dir.joinpath(self.output_file(output))
 
         with TempDir(output_dir) as tmp_dir:
-            tmp_file = tmp_dir.joinpath("output.pdf")
-
-            [_, _, process_result] = self.run_process(
-                self.dependencies["chromium"][0],
-                "--headless",
-                "--no-sandbox",
-                f"--print-to-pdf={tmp_file}",
-                "--no-pdf-header-footer",
-                self.file.get_absolute_path(),
-                cwd=tmp_dir,
-            )
-
-            if not tmp_file.is_file():
-                raise ConvertError(
-                    self.file,
-                    "Output file not found.",
-                    CalledProcessError(
-                        process_result.returncode,
-                        process_result.args,
-                        process_result.stdout,
-                        process_result.stderr,
-                    ),
-                )
+            HTML(filename=self.file.get_absolute_path()).write_pdf(tmp_file := tmp_dir.joinpath(dest_file.name))
 
             dest_dir.mkdir(parents=True, exist_ok=True)
 
