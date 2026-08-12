d4b9c0b1791dd694ded90c66a04e452f192b0f9f
Matteo Campinoti <matteo.campinoti94@gmail.com>
2026-08-12 11:14:19 +0200

    converters.html,.vector - use weasy print
    Merge from main

--- src/convertool/converters/converter_html.py
+++ src/convertool/converters/converter_html.py
@@ -1,11 +1,18 @@
 from pathlib import Path
 from typing import ClassVar
 
-from weasyprint import HTML
-
 from convertool.util import TempDir
 
 from .base import ConverterABC
+from .exceptions import MissingDependency
+
+try:
+    import weasyprint
+
+    weasyprint_error: Exception | None = None
+except (ImportError, OSError) as e:
+    weasyprint = None
+    weasyprint_error: Exception | None = e
 
 
 class HTMLConverter(ConverterABC):
@@ -13,19 +20,27 @@ class HTMLConverter(ConverterABC):
     outputs: ClassVar[list[str]] = ["pdf"]
 
     @classmethod
-    def output_name(cls, output: str) -> str:  # noqa: ARG003
-        return "pdf"
-
-    def output_extension(self, output: str) -> str:  # noqa: ARG002
-        return ".pdf"
+    def test_dependencies(cls):
+        if weasyprint is None:
+            raise MissingDependency(["weasyprint"], weasyprint_error or "missing system dependencies")
+        super().test_dependencies()
 
     def convert(self, output_dir: Path, output: str, *, keep_relative_path: bool = True) -> list[Path]:
         self.test_output(output)
+
+        if weasyprint is None:
+            raise MissingDependency(["weasyprint"], weasyprint_error or "missing system dependencies")
+
         dest_dir: Path = self.output_dir(output_dir, keep_relative_path=keep_relative_path)
         dest_file: Path = dest_dir.joinpath(self.output_file(output))
 
         with TempDir(output_dir) as tmp_dir:
-            HTML(filename=self.file.get_absolute_path()).write_pdf(tmp_file := tmp_dir.joinpath(dest_file.name))
+            html = weasyprint.HTML(
+                filename=self.file.get_absolute_path(),
+                encoding=(self.file.encoding["encoding"] or "") if self.file.encoding else "",
+            )
+
+            html.write_pdf(tmp_file := tmp_dir.joinpath(dest_file.name))
 
             dest_dir.mkdir(parents=True, exist_ok=True)
 
--- src/convertool/converters/converter_vector.py
+++ src/convertool/converters/converter_vector.py
@@ -1,55 +1,46 @@
 from pathlib import Path
-from subprocess import CalledProcessError
 from typing import ClassVar
 
 from convertool.util import TempDir
 
 from .base import ConverterABC
-from .exceptions import ConvertError
+from .exceptions import MissingDependency
+
+try:
+    import weasyprint
+
+    weasyprint_error: Exception | None = None
+except (ImportError, OSError) as e:
+    weasyprint = None
+    weasyprint_error: Exception | None = e
 
 
 class VectorConverter(ConverterABC):
     name: ClassVar[str] = "vector"
     outputs: ClassVar[list[str]] = ["pdf"]
-    dependencies: ClassVar[dict[str, list[str]]] = {"chromium": ["chromium", "chromium-browser", "google-chrome"]}
-    process_timeout: ClassVar[int] = 60
 
     @classmethod
-    def output_name(cls, output: str) -> str:
-        return "pdf"
-
-    def output_extension(self, output: str) -> str:
-        return ".pdf"
+    def test_dependencies(cls):
+        if weasyprint is None:
+            raise MissingDependency(["weasyprint"], weasyprint_error or "missing system dependencies")
+        super().test_dependencies()
 
     def convert(self, output_dir: Path, output: str, *, keep_relative_path: bool = True) -> list[Path]:
         self.test_output(output)
+
+        if weasyprint is None:
+            raise MissingDependency(["weasyprint"], weasyprint_error or "missing system dependencies")
+
         dest_dir: Path = self.output_dir(output_dir, keep_relative_path=keep_relative_path)
         dest_file: Path = dest_dir.joinpath(self.output_file(output))
 
         with TempDir(output_dir) as tmp_dir:
-            tmp_file = tmp_dir.joinpath("output.pdf")
-
-            [_, _, process_result] = self.run_process(
-                self.dependencies["chromium"][0],
-                "--headless",
-                "--no-sandbox",
-                f"--print-to-pdf={tmp_file}",
-                "--no-pdf-header-footer",
-                self.file.get_absolute_path(),
-                cwd=tmp_dir,
+            html = weasyprint.HTML(
+                filename=self.file.get_absolute_path(),
+                encoding=(self.file.encoding["encoding"] or "") if self.file.encoding else "",
             )
 
-            if not tmp_file.is_file():
-                raise ConvertError(
-                    self.file,
-                    "Output file not found.",
-                    CalledProcessError(
-                        process_result.returncode,
-                        process_result.args,
-                        process_result.stdout,
-                        process_result.stderr,
-                    ),
-                )
+            html.write_pdf(tmp_file := tmp_dir.joinpath(dest_file.name))
 
             dest_dir.mkdir(parents=True, exist_ok=True)
 
