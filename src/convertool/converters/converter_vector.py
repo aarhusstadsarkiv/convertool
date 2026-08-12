4937ef437b989ea23ab36dcb8a2097f8e3173c61
Matteo Campinoti <matteo.campinoti94@gmail.com>
2026-08-12 12:39:31 +0200

    converters.vector - alias `HTMLConverter`

--- src/convertool/converters/converter_vector.py
+++ src/convertool/converters/converter_vector.py
@@ -1,49 +1,7 @@
-from pathlib import Path
 from typing import ClassVar
 
-from convertool.util import TempDir
-
-from .base import ConverterABC
-from .exceptions import MissingDependency
-
-try:
-    import weasyprint
-
-    weasyprint_error: Exception | None = None
-except (ImportError, OSError) as e:
-    weasyprint = None
-    weasyprint_error: Exception | None = e
+from .converter_html import HTMLConverter
 
 
-class VectorConverter(ConverterABC):
+class VectorConverter(HTMLConverter):
     name: ClassVar[str] = "vector"
-    outputs: ClassVar[list[str]] = ["pdf"]
-
-    @classmethod
-    def test_dependencies(cls):
-        if weasyprint is None:
-            raise MissingDependency(["weasyprint"], weasyprint_error or "missing system dependencies")
-        super().test_dependencies()
-
-    def convert(self, output_dir: Path, output: str, *, keep_relative_path: bool = True) -> list[Path]:
-        self.test_output(output)
-
-        if weasyprint is None:
-            raise MissingDependency(["weasyprint"], weasyprint_error or "missing system dependencies")
-
-        dest_dir: Path = self.output_dir(output_dir, keep_relative_path=keep_relative_path)
-        dest_file: Path = dest_dir.joinpath(self.output_file(output))
-
-        with TempDir(output_dir) as tmp_dir:
-            html = weasyprint.HTML(
-                filename=self.file.get_absolute_path(),
-                encoding=(self.file.encoding["encoding"] or "") if self.file.encoding else "",
-            )
-
-            html.write_pdf(tmp_file := tmp_dir.joinpath(dest_file.name), options={"pdf_variant": "pdf/a-4u"})
-
-            dest_dir.mkdir(parents=True, exist_ok=True)
-
-            tmp_file.replace(dest_file)
-
-        return [dest_file]
