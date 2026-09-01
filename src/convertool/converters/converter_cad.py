0696aed71f61dad2cf56497224ae0e40881a93d0
Matteo Campinoti <matteo.campinoti94@gmail.com>
2026-09-01 12:37:41 +0200

    converters.cad - escape temporary directory path using `shlex`

--- src/convertool/converters/converter_cad.py
+++ src/convertool/converters/converter_cad.py
@@ -1,3 +1,4 @@
+import shlex
 from pathlib import Path
 from typing import ClassVar
 
@@ -50,7 +51,7 @@ class CADConverter(ConverterABC):
                 self.dependencies["abviewer"][0],
                 "/c",
                 output,
-                f"dir={tmp_dir}",
+                f"dir={shlex.quote(str(tmp_dir))}",
                 self.file.get_absolute_path(),
             )
             dest_dir.mkdir(parents=True, exist_ok=True)
